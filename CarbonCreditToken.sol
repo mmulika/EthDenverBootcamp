353d847a55423cbf9bc570b5414cfb7f4952f532
mmulika <mulikamike@gmail.com>
2023-01-04 17:54:03 +0300

    struct

--- CarbonCreditToken.sol
+++ CarbonCreditToken.sol
@@ -9,21 +9,43 @@ contract CarbonCreditToken{
     address owner;
     uint totalSupply = 1000;
     uint supply;
+    event  added(uint);
+    event SetSupply(uint);
+    mapping( address => uint) _follow;
+
+    struct Team{
+
+        address  account;
+        uint ID;
+    }
 
 
+
+   modifier ownerOnly{
+       if(msg.sender == owner){
+           _;
+       }
+       
+   }
+
+constructor(){
+    msg.sender == owner;
+}
 
     function getSupply() public view  returns (uint){
         return totalSupply;
     }
-    function add( uint a, uint b) public{
+    function add( uint a, uint b) public {
         uint sum;
         sum = a + b;
+        emit added(sum);
         
     }
 
-    function setSupply( uint  _supply) public {
+    function setSupply(uint  _supply) public ownerOnly{
     
         supply = _supply ;
+        emit SetSupply(_supply);
         
     }
 
