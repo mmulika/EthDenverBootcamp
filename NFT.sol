3e5ebe37537e54eacc8dc27e2cdf6486050bf5de
mmulika <mulikamike@gmail.com>
2023-01-06 14:08:31 +0300

    setGreenToken

--- NFT.sol
+++ NFT.sol
@@ -28,4 +28,29 @@ contract NFT  is ERC721URIStorage, Ownable{
     }
 
 
+  struct GreenToken{
+    address  owner;
+    uint256 value;
+
+  }
+
+  GreenToken public greenToken;
+  event GreenTokenCreated ( address);
+
+
+  function getGreenToken() public view returns (address){
+    return  greenToken.owner;
+
+  }
+
+  function setGreenToken(address _add, uint256 _amount) public{
+    greenToken.owner = _add;
+    greenToken.value = _amount;
+    emit GreenTokenCreated( _add);
+  }
+
+
+
+  
+
 }
