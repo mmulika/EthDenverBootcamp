3858d599f78fa0e38041f4efdc6c7f7761fb2dc5
mmulika <mulikamike@gmail.com>
2023-01-09 07:01:25 +0300

    erc20

---
+++ ERC20.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+
+
+constract ERC20  is ERC20,payable,transfarable{
+    
+ address public royaltyAddress;
+ uint256 public royaltyAmount;
+
+ constructor ()
+
+ }
+
+
+}
