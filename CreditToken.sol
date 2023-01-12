41fae6fef3e0cad102d9f22372edf2fa253c9198
mmulika <mulikamike@gmail.com>
2023-01-12 11:17:37 +0300

    tokenupdate

--- CreditToken.sol
+++ CreditToken.sol
@@ -9,13 +9,13 @@ import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract CreditToken is  CarbonCredit,ERC20{
     uint totalsupply;
-    uint supply;
+    uint supply = 1000000;
     uint tokenBurnedCount = 0;
     uint private _decimals;
 
 
-    constructor() ERC20("OkoaNature","OKA") public {
-        _setupDecimals(0);
+    constructor() ERC20 ("OkoaNature","OKA") {
+        _mint(msg.sender, totalsupply);
     
 
     }
@@ -24,7 +24,7 @@ contract CreditToken is  CarbonCredit,ERC20{
     mapping(address => uint)  public approvedCredits;
     mapping(address => uint) public  totalApprovedForBurn;
     
-    modifier onlyVerifier(){
+    modifier onlyVerifier() { 
         require(msg.sender == verifiers[verifiercount].addr);
         _;
     }
@@ -41,47 +41,50 @@ contract CreditToken is  CarbonCredit,ERC20{
 
     function createCarbonToken()public onlyCreditHolder()returns(uint){
         address Owner = CreditHolders[totalRegistered]._add;
+        
         supply = approvedCredits[Owner];
-        totalsupply = totalsupply.add(supply);
-        BalancesOf[Owner] = BalancesOf[Owner].add(supply);
+        totalsupply = totalsupply + (supply);
+        BalancesOf[Owner] = BalancesOf[Owner] + (supply);
         _mint(msg.sender, supply);
         return totalsupply;
     }
 
-    function transferCredits(address to, uint value) public {
-        onlyCreditHolder(){
+    function transferCredits(address to, uint  value) public  onlyCreditHolder(){
+        {
             _transfer(msg.sender, to, value);
-            BalancesOf[msg.sender] =BalancesOf[msg.sender].sub(value);
-            BalancesOf[to] = BalancesOf[to].add(value);
+            BalancesOf[msg.sender] =BalancesOf[msg.sender]- (value);
+            BalancesOf[to] = BalancesOf[to] + (value);
         }
     }
 
     function transferCreditsFrom( address from, address to, uint value) public{
         transferFrom(from, to, value);
-        BalanceOf[from] = BalanceOf[from].sub(value);
-        BalanceOf[to] = BalanceOf[to].add(value);
+        BalancesOf[from] = BalancesOf[from] - (value);
+        BalancesOf[to] = BalancesOf[to] + (value);
 
     }
 
-    function approveBurn(uit carbonTokens,address holder)public{
-        onlyVerifier() {
-            tokensApprovedForBurn[holder]+=carbonTokens;
+    function approveBurn(uint carbonTokens,address holder)public  onlyVerifier() {
+       {
+            totalApprovedForBurn[holder]+=carbonTokens;
 
         }
 
-function  burnTokens() public onlyOwner returns(bool){
-    _burn(msg.sender,tokensApprovedForBurn[msg.sender]);
-    totalsupply =  totalsupply.sub(tokensApprovedForBurn[msg.sender]);
-    BalanceOf[msg.sender] = BalanceOf[msg.sender].sub(tokensApprovedForBurn[msg.sender]);
-        return true;
-}
+// function  burnTokens() public onlyOwner() returns(bool){
+//     _burn(msg.sender,tokensApprovedForBurn[msg.sender]);
+//     totalsupply =  totalsupply.sub(tokensApprovedForBurn[msg.sender]);
+//     BalanceOf[msg.sender] = BalanceOf[msg.sender].sub(tokensApprovedForBurn[msg.sender]);
+//         return true;
+// }
 
-      function buyCarbonCredits(uint amount) public payable {
-        uint value = amount * 1;
-        require(msg.value > 0);
-        transferCreditsFrom(address(this),msg.sender, value);
-        }
+    //   function buyCarbonCredits(uint amount) public payable {
+    //     uint value = amount * 1;
+    //     require(msg.value > 0);
+    //     transferCreditsFrom(address(this),msg.sender, value);
+    //     }
 
+    // }
     
 }
 
+}
