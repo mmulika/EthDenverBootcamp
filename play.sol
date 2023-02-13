619889791e16a3c1b5416a6614a29593413cbe7e
mmulika <mulikamike@gmail.com>
2023-02-13 23:10:30 +0300

    earnasplay

---
+++ play.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract EarnAsYouPlay {
+    mapping(address => uint256) public points;
+    uint256 public reward;
+
+    function earnPoints(uint256 earnedPoints) public {
+        points[msg.sender] += earnedPoints;
+    }
+
+    function redeemPoints() public {
+        require(points[msg.sender] >= reward, "You do not have enough points to redeem a reward");
+        msg.sender.transfer(reward);
+        points[msg.sender] -= reward;
+    }
+}
