47a509f065f84e1588129bdbcdf5362dc3153823
mmulika <mulikamike@gmail.com>
2023-03-20 18:09:05 +0300

    update

--- CarbonCreditToken.sol
+++ CarbonCreditToken.sol
@@ -37,6 +37,11 @@ struct salary{
     string job;
     uint  net;
 }
+
+mapping (uint -> salary) ear/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+/// @return Documents the return variables of a contract’s function state variable
+/// @inheritdoc	Copies all missing tags from the base function (must be followed by the contract name)
 mapping  (uint -> kenya) balance
 
 struct Email{
